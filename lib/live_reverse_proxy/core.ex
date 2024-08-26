af38d13b7d1adb36158dc978d4645f1f5d3a4b46
Tsutomu Kuroda <t-kuroda@oiax.jp>
2024-08-26 09:22:19 +0900

    Rename tgweb to vhosts

--- lib/live_reverse_proxy/core.ex
+++ lib/live_reverse_proxy/core.ex
@@ -2,11 +2,10 @@ defmodule LiveReverseProxy.Core do
   alias LiveReverseProxy.Core
   alias LiveReverseProxy.Repo
 
-  @virtual_host_dir Path.expand(Path.join(__DIR__, "../../tgweb/sites"))
+  @virtual_host_dir Application.compile_env(:live_reverse_proxy, :virtual_hosts_dir) ||
+                      Path.expand(Path.join(__DIR__, "../../vhosts/sites"))
 
-  def virtual_hosts_dir do
-    Application.get_env(:live_reverse_proxy, :virtual_hosts_dir) || @virtual_host_dir
-  end
+  def virtual_hosts_dir, do: @virtual_host_dir
 
   def get_virtual_host(fqdn) do
     Repo.get_by(Core.VirtualHost, fqdn: fqdn)

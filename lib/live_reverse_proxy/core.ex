c351b76cdd6541875728ff73368cc967518f0bb2
Tsutomu Kuroda <t-kuroda@oiax.jp>
2024-08-28 17:18:11 +0900

    [admin] List owners, virtual_hosts and servers

--- lib/live_reverse_proxy/core.ex
+++ lib/live_reverse_proxy/core.ex
@@ -3,6 +3,30 @@ defmodule LiveReverseProxy.Core do
   alias LiveReverseProxy.Repo
   import Ecto.Query, only: [from: 2]
 
+  def get_owners do
+    from(o in Core.Owner,
+      preload: [virtual_hosts: :servers],
+      order_by: o.name
+    )
+    |> Repo.all()
+  end
+
+  def get_virtual_hosts do
+    from(vh in Core.VirtualHost,
+      preload: [:owner, :servers],
+      order_by: vh.code_name
+    )
+    |> Repo.all()
+  end
+
+  def get_servers do
+    from(s in Core.Server,
+      preload: [virtual_host: :owner],
+      order_by: s.fqdn
+    )
+    |> Repo.all()
+  end
+
   @virtual_host_dir Application.compile_env(:live_reverse_proxy, :virtual_hosts_dir) ||
                       Path.expand(Path.join(__DIR__, "../../vhosts/sites"))
 
--- lib/live_reverse_proxy_web/live/admin_live.ex
+++ lib/live_reverse_proxy_web/live/admin_live.ex
@@ -1,12 +1,14 @@
 defmodule LiveReverseProxyWeb.AdminLive do
   use LiveReverseProxyWeb, :live_view
+  alias LiveReverseProxy.Core
 
   embed_templates "admin_live/*"
 
-  def render(%{current_section_name: "dashboard"} = assigns), do: ~H"<.dashboard assigns/>"
-  def render(%{current_section_name: "owners"} = assigns), do: ~H"<.owners assigns/>"
-  def render(%{current_section_name: "virtual_hosts"} = assigns), do: ~H"<.virtual_hosts assigns/>"
-  def render(%{current_section_name: "servers"} = assigns), do: ~H"<.servers assigns/>"
+  def render(%{current_section_name: "dashboard"} = assigns), do: ~H"<.dashboard/>"
+  def render(%{current_section_name: "owners"} = assigns), do: ~H"<.owners owners={@owners}/>"
+  def render(%{current_section_name: "virtual_hosts"} = assigns),
+    do: ~H"<.virtual_hosts virtual_hosts={@virtual_hosts}/>"
+  def render(%{current_section_name: "servers"} = assigns), do: ~H"<.servers servers={@servers}/>"
 
   def mount(_params, _session, socket) do
     socket =
@@ -16,10 +18,35 @@ defmodule LiveReverseProxyWeb.AdminLive do
     {:ok, socket}
   end
 
-  @available_section_names ~w(dashboard owners virtual_hosts servers)a
+  def handle_params(_params, _uri, socket) when socket.assigns.live_action == :dashboard do
+    socket = assign(socket, :current_section_name, "dashboard")
+    {:noreply, socket}
+  end
 
-  def handle_params(_params, _uri, socket) when socket.assigns.live_action in @available_section_names do
-    socket = assign(socket, :current_section_name, Atom.to_string(socket.assigns.live_action))
+  def handle_params(_params, _uri, socket) when socket.assigns.live_action == :owners do
+    socket =
+      socket
+      |> assign(:current_section_name, "owners")
+      |> assign(:owners, Core.get_owners())
+
+    {:noreply, socket}
+  end
+
+  def handle_params(_params, _uri, socket) when socket.assigns.live_action == :virtual_hosts do
+    socket =
+      socket
+      |> assign(:current_section_name, "virtual_hosts")
+      |> assign(:virtual_hosts, Core.get_virtual_hosts())
+
+    {:noreply, socket}
+  end
+
+  def handle_params(_params, _uri, socket) when socket.assigns.live_action == :servers do
+    socket =
+      socket
+      |> assign(:current_section_name, "servers")
+      |> assign(:servers, Core.get_servers())
+
     {:noreply, socket}
   end
 end

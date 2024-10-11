ddfad13f71404cd831d101ac2a82521c6e81b794
Kanase Kazuyoshi <k.kanase1547@outlook.jp>
2024-10-11 22:34:16 +0900

    Fixed hash password salt

--- priv/repo/seeds/dev/administrators.exs
+++ priv/repo/seeds/dev/administrators.exs
@@ -10,7 +10,7 @@ usernames = ~w(
 for {username, index} <- Enum.with_index(usernames) do
   administrator =
     Repo.insert!(%Core.Administrator{
-      password_hash: Bcrypt.hash_pwd_salt(username <> "888"),
+      password_hash: Bcrypt.hash_pwd_salt(username <> "8888"),
       superadmin: index == 0
     })
 

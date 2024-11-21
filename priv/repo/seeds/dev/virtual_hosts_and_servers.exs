f5fc804e6f43d9be086b2240784734763d95ea30
2814109 <2814109k@gmail.com>
2024-11-22 03:34:18 +0900

    fix seed file

--- priv/repo/seeds/dev/virtual_hosts_and_servers.exs
+++ priv/repo/seeds/dev/virtual_hosts_and_servers.exs
@@ -3,21 +3,26 @@ use Timex
 alias LiveWebServer.Repo
 alias LiveWebServer.Core
 
+today_utc = Timex.now("Etc/UTC") |> Timex.beginning_of_day()
+
+
 virtual_host_tuples = [
-  {"Kappa", "alpha", nil},
-  {"Kappa", "beta", "https://www.google.co.jp/"},
-  {"Lambda", "gamma", "https://www.yahoo.co.jp/"}
+  {"Kappa", "epsilon", nil,today_utc},
+  {"Kappa", "alpha", nil,nil},
+  {"Kappa", "beta", "https://www.google.co.jp/",nil},
+  {"Lambda", "gamma", "https://www.yahoo.co.jp/",nil}
 ]
 
 
-for {owner_name, hostname, target} <- virtual_host_tuples do
+for {owner_name, hostname, target,expired_at} <- virtual_host_tuples do
   owner = Core.get_owner_by_name(owner_name)
 
   vh =
     Repo.insert!(%Core.VirtualHost{
       owner: owner,
       code_name: hostname,
-      redirection_target: target
+      redirection_target: target,
+      expired_at: expired_at
     })
 
   Repo.insert!(%Core.Server{
@@ -25,18 +30,3 @@ for {owner_name, hostname, target} <- virtual_host_tuples do
     fqdn: "#{hostname}.lvh.me"
   })
 end
-
-owner = Core.get_owner_by_name("Kappa")
-today_utc = Timex.now("Etc/UTC") |> Timex.beginning_of_day()
-
-Repo.insert!(%Core.VirtualHost{
-  owner: owner,
-  code_name: "epsilon",
-  redirection_target: nil,
-  expired_at: today_utc
-})
-
-Repo.insert!(%Core.Server{
-  virtual_host: Repo.get_by!(Core.VirtualHost, code_name: "epsilon"),
-  fqdn: "epsilon.lvh.me"
-})
